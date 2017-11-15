a933734742f1dac5fa3233db5de1f29002159b29
Ilhom Madrahimov <madrahimov.ilhom@gmail.com>
2017-11-15 16:36:47 +0300

    fix

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ApplicationRecord
   has_many :project_languages
   has_many :languages, through: :project_languages
   has_many :words
-  
+  belongs_to :default_language, class_name: 'Language'
 end
